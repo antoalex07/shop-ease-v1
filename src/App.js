829ffba512a4011882617c70f5caf3fc1a0dba4e
antoalex07 <antoalex710@gmail.com>
2024-05-03 22:38:39 +0530

    set up route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Authwrapper from './auth/Authwrapper';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Authwrapper/>
+      </BrowserRouter>
     </div>
   );
 }
