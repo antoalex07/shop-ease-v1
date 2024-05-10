7c21578e611743e82247d3e88e3f9d0a5d1b853c
antoalex07 <antoalex710@gmail.com>
2024-05-10 22:54:38 +0530

    logo added

--- src/components/structure/header/Header.js
+++ src/components/structure/header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./Header.css"
 
 const RenderHeader = () => {
   return (
     <div className='header'>
-        
+        <div className='logo-container'></div>
     </div>
   )
 }
